a14e6e462f3aacb174bbdece1050f0e5875e6f61
João VItor <jvitordiass@outlook.com>
2022-12-11 21:32:55 -0300

    feat: firebase auth service

---
+++ src/shared/firebase/index.ts
@@ -0,0 +1,12 @@
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { addDoc, collection } from 'firebase/firestore'
+import { authConfig, db } from '../environment'
+
+export const login = async (email: string, password: string): Promise<void> => {
+  await signInWithEmailAndPassword(authConfig, email, password)
+} 
+
+export const register = async (email: string, password: string): Promise<void> => {
+  const res = await createUserWithEmailAndPassword(authConfig, email, password)
+  await addDoc(collection(db, 'users'), { user: res.user.uid })
+} 
