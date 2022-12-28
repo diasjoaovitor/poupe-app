a38611eee885b223af729c598e03184fe81e5493
João VItor <jvitordiass@outlook.com>
2022-12-27 23:06:34 -0300

    feat: Period

---
+++ src/shared/components/Period/index.tsx
@@ -0,0 +1,38 @@
+import { MenuItem, Select, SelectChangeEvent, Stack } from '@mui/material'
+import { months } from '../../states'
+import * as S from './style'
+
+type Props = {
+  month: string
+  year: string | number
+  years: (string | number)[]
+  handleChange(e: SelectChangeEvent): void
+}
+
+export const Period: React.FC<Props> = ({ 
+  month, year, years,
+  handleChange
+}) => {
+  return (
+    <Stack className="Period" component="section" direction="row" sx={S.period}>
+      <Select
+        name="month"
+        value={month}
+        onChange={handleChange}
+      >
+        {months.map(month => (
+          <MenuItem key={month} value={month}>{month}</MenuItem>
+        ))}
+      </Select>
+      <Select
+        name="year"
+        value={String(year)}
+        onChange={handleChange}
+      >
+        {years.map(year => (
+          <MenuItem key={year} value={year}>{year}</MenuItem>
+        ))}
+      </Select>
+    </Stack>
+  )
+}
---
+++ src/shared/components/Period/style.ts
@@ -0,0 +1,6 @@
+import { SxProps, Theme } from '@mui/material'
+
+export const period: SxProps<Theme> = {
+  justifyContent: 'center',
+  columnGap: 2
+}
