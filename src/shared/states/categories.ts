feae257bcc1d9005fb26befeb997aa0bb72115e3
João Vitor <jvitordiass@outlook.com>
2022-12-29 17:31:45 -0300

    feat: categories state

---
+++ src/shared/states/categories.ts
@@ -0,0 +1,98 @@
+import { AccountBalance, AttachMoney, BeachAccess, DriveEta, FitnessCenter, HeartBroken, LinearScale, LocalAirport, LocalAtm, LocalHospital, OndemandVideo, Redeem, School, Score, Shop, ShoppingCart, TrendingUp } from '@mui/icons-material'
+
+export const expenseCategories = [
+  {
+    name: 'Aposta',
+    icon: Score
+  },
+  {
+    name: 'Assinaturas',
+    icon: OndemandVideo
+  },
+  {
+    name: 'Compras',
+    icon: Shop
+  },
+  {
+    name: 'Educação',
+    icon: School
+  },
+  {
+    name: 'Esporte',
+    icon: FitnessCenter
+  },
+  {
+    name: 'Investimentos',
+    icon: TrendingUp
+  },
+  {
+    name: 'Lazer',
+    icon: BeachAccess
+  },
+  {
+    name: 'Mercado',
+    icon: ShoppingCart
+  },
+  {
+    name: 'Saúde',
+    icon: LocalHospital
+  },
+  {
+    name: 'Transporte',
+    icon: DriveEta
+  },
+  {
+    name: 'Viagem',
+    icon: LocalAirport
+  },
+  {
+    name: 'Outros',
+    icon: LinearScale
+  }
+]
+
+export const incomeCategories = [
+  {
+    name: 'Bônus',
+    icon: AttachMoney
+  },
+  {
+    name: 'Comissão',
+    icon: AccountBalance
+  },
+  {
+    name: 'Ivestimentos',
+    icon: TrendingUp
+  },
+  {
+    name: 'Presente',
+    icon: Redeem
+  },
+  {
+    name: 'Salário',
+    icon: LocalAtm
+  },
+  {
+    name: 'Outros',
+    icon: LinearScale
+  }
+]
+
+export const recorrencyOptions = [
+  {
+    name: 'Não Repetir',
+    value: 'Não Repetir'
+  },
+  {
+    name: 'Semanal',
+    value: 'WEEKLY'
+  },
+  {
+    name: 'Mensal',
+    value: 'MONTHLY'
+  },
+  {
+    name: 'Anual',
+    value: 'YEARLY'
+  },
+]
