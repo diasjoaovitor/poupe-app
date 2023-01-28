a8c9c73bdbd381bab5ad1508d2d61156322f2662
João Vitor <jvitordiass@outlook.com>
2023-01-28 16:21:00 -0300

    feat: defaultAppState and transaction states

--- src/shared/states/index.ts
+++ src/shared/states/index.ts
@@ -1,16 +1,34 @@
+import { TAppData, TTransaction } from "../types"
+
+export const formatZero = (zero: number) => zero < 10 ? `0${zero}` : zero
+
 const date = new Date()
 
 export const months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
 
-export const year= date.getFullYear() as string | number
+export const year = date.getFullYear() as string | number
 
-export const period = {
-  year: date.getFullYear(),
-  month: months[date.getMonth()],  
+export const defaultAppState: TAppData = {
+  period: {
+    year: date.getFullYear(),
+    month: months[date.getMonth()],  
+  },
+  years: [year],
+  wallet: {
+    balance: 0,
+    incomes: 0,
+    expenses: 0
+  },
+  transactions: [] as TTransaction[],
+  transaction: undefined
 }
 
-export const wallet = {
-  balance: 0,
-  incomes: 0,
-  expenses: 0
+export const transaction: TTransaction = {
+  ref: '',
+  period: '',
+  type: 'Despesa',
+  category: 'Outros',
+  description: '',
+  value: 0,
+  date: `${date.getFullYear()}/${formatZero(date.getMonth() + 1)}/${formatZero(date.getDate())}`
 }
