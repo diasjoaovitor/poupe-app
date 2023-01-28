a406b126603d95f2f1e6985ca2b6593637f7c977
João Vitor <jvitordiass@outlook.com>
2023-01-28 16:23:11 -0300

    test: <Category />

---
+++ src/tests/components/category.spec.tsx
@@ -0,0 +1,44 @@
+import { SvgIconComponent } from "@mui/icons-material"
+import { fireEvent, render, screen } from "@testing-library/react"
+import { Category } from "../../shared/components"
+import { incomeCategories } from "../../shared/states/categories"
+
+type TCategories = {
+  name: string
+  icon: SvgIconComponent
+}[]
+
+const getDefaultSelected = (radiogroup: HTMLElement) => (
+  radiogroup.querySelector('input:checked') as HTMLInputElement
+)
+
+const setup = (category: string, categories: TCategories) => {
+  render(
+    <Category category={category} categories={categories} color={{ hex: '#d32f2f', mui: 'error' }} />
+  )
+  const radiogroup = screen.getByRole('radiogroup')
+  return radiogroup
+}
+
+describe('<Category />', () => {
+  it('Default Category if not exists', () => {
+    const radiogroup = setup('not exists category', incomeCategories)
+    const defaultSelected = getDefaultSelected(radiogroup)
+
+    expect(defaultSelected.value).toBe('Outros')
+    expect(defaultSelected.parentElement).toHaveStyle('color: #d32f2f')
+  })
+
+  it('Select Category', () => {
+    const radiogroup = setup('Outros', incomeCategories)
+    const radioInputs = radiogroup.querySelectorAll('input[type=radio]')
+
+    const radio = Array.from(radioInputs).find(radio => (
+      (radio as HTMLInputElement).value === 'Salário'
+    )) as HTMLInputElement
+    expect(radio.parentElement).not.toHaveStyle('color: #d32f2f')
+
+    fireEvent.click(radio)
+    expect(radio.parentElement).toHaveStyle('color: #d32f2f')
+  })
+})
