463fa4785539cec88c2f87d6a79c1bfe347b8b84
João Vitor <jvitordiass@outlook.com>
2023-01-28 16:09:29 -0300

    feat: <Type /> component

---
+++ src/shared/components/Type/index.tsx
@@ -0,0 +1,27 @@
+import { ChangeEvent } from 'react'
+import { FormControl, FormControlLabel, Radio, RadioGroup, Typography } from '@mui/material'
+import { TTransactionType } from '../../types'
+import * as S from './style'
+
+type Props = {
+  type: TTransactionType
+  handleTypeChange(e: ChangeEvent<HTMLInputElement>): void
+}
+
+export const Type: React.FC<Props> = ({ type, handleTypeChange }) => {
+  return (
+    <FormControl sx={S.transactionType}>
+      <Typography component="label">Tipo de lançamento</Typography>
+      <RadioGroup value={type}>
+        <FormControlLabel 
+          label="Despesa" value="Despesa" name="type"
+          control={<Radio color="error" onChange={handleTypeChange} />}  
+        />
+        <FormControlLabel 
+          label="Receita" value="Receita" name="type"
+          control={<Radio onChange={handleTypeChange} />} 
+        />
+      </RadioGroup>
+    </FormControl>
+  )
+}
---
+++ src/shared/components/Type/style.ts
@@ -0,0 +1,8 @@
+import { SxProps, Theme } from '@mui/material'
+
+export const transactionType: SxProps<Theme> = {
+  padding: 2,
+  '& .MuiFormGroup-root': {
+    flexDirection: 'row'
+  }
+}
