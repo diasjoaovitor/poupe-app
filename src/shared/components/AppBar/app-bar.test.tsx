d7f67a6883bc2b6efd56ac8d06db179335402e76
João VItor <jvitordiass@outlook.com>
2023-03-01 22:05:54 -0300

    refactor: move test to component folder

---
+++ src/shared/components/AppBar/app-bar.test.tsx
@@ -0,0 +1,43 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { createMemoryRouter, RouterProvider } from 'react-router-dom'
+import { AppBar } from '.'
+
+const setup = (md: boolean) => {
+  const router = createMemoryRouter(
+    [
+      { path: '/app-bar', element: <AppBar page="App" md={md} /> },
+      { path: '/login', element: <></> },
+      { path: '/', element: <></> },
+    ],
+    {
+      initialEntries: ['/app-bar']
+    }
+  )
+  render(<RouterProvider router={router} />)
+  return router
+}
+
+const getLinks = () => screen.queryAllByRole('link')
+
+describe('<AppBar />', () => {
+  it('Menu', () => {
+    setup(false)
+    expect(getLinks()).toHaveLength(0)
+    const menu = screen.getByRole('menu')
+    fireEvent.click(menu)
+    expect(getLinks()).toHaveLength(3)
+    fireEvent.click(menu)
+    expect(getLinks()).toHaveLength(0)
+    setup(true)
+    expect(getLinks()).toHaveLength(3)
+  }) 
+  
+  it('Navigate', () => {
+    const router = setup(false)
+    const menu = screen.getByRole('menu')
+    fireEvent.click(menu)
+    const dashboard = screen.getByText('Dashboard')
+    fireEvent.click(dashboard)
+    expect(router.state.location.pathname).toBe('/')
+  })
+})
