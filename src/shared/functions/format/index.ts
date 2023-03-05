f862ecfe1671d6a5506ffbfca5d18453166f3bd8
João VItor <jvitordiass@outlook.com>
2023-03-05 16:10:49 -0300

    refactor: format functions

--- src/shared/functions/format/index.ts
+++ src/shared/functions/format/index.ts
@@ -1,6 +1,8 @@
 import { months } from '../../states'
 import { TTransaction } from '../../types'
 
+export const formatZero = (n: number) => n > 0 && n < 10 ? `0${n}` : n
+
 export const formatCurrency = (value: number) => (
   new Intl.NumberFormat('pt-BR', {
     style: 'currency',
@@ -12,14 +14,16 @@ export const formatPresentationOfTransactions = (transactions: TTransaction[]) =
   const allDates = transactions.map(({ date }) => date)
   const dates = Array.from(new Set(allDates))
   const presentation = dates.map(date => {
-    const [ _, month, day ] = date.split('/') 
+    const d = new Date(date)
+    const month = d.getMonth() + 1
+    const day = d.getDate()
     const m = months[Number(month) - 1].slice(0, 3).toLocaleLowerCase()
     const filteredTransactions = transactions.filter(posting => posting.date === date)
     const total = filteredTransactions.reduce((count, { value, type }) => {
       return type === 'Despesa' ? count -= value : count += value
     }, 0)
     return {
-      date: `${day}/${m}`,
+      date: `${formatZero(day)}/${m}`,
       total,
       transactions: filteredTransactions
     }
@@ -28,7 +32,9 @@ export const formatPresentationOfTransactions = (transactions: TTransaction[]) =
 }
 
 export const formatDate = (date: string) => {
-    if (!date) return date
-    const [ year, month, day ] = date.split('/') 
-    return `${day}/${month}/${year}`
+  const d = new Date(date)
+  const day = formatZero(d.getDate())
+  const month = formatZero(d.getMonth() + 1)
+  const year = d.getFullYear()
+  return `${day}/${month}/${year}`
 }
