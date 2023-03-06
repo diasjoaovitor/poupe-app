e15c812fc8f5ee1bfe4c7004081b68b2263669ef
João VItor <jvitordiass@outlook.com>
2023-03-05 23:21:05 -0300

    test: <Notification />

---
+++ src/shared/components/Notification/notification.test.tsx
@@ -0,0 +1,16 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { Notification } from '.'
+
+describe('<Notification />', () => {
+  it('Close notification modal', async () => {
+    const message = "I'm visible"
+    render(<Notification message={message} />)
+    const notificationMessage = screen.queryByText(message)
+    expect(notificationMessage).toBeInTheDocument()
+    const button = screen.getByRole('button')
+    fireEvent.click(button)
+    await waitFor(() => {
+      expect(notificationMessage).not.toBeInTheDocument()
+    })
+  }) 
+})
