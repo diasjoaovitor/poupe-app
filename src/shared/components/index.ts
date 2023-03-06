0eeb66081c7fcbe32b14f7613f2c986ca3d778c3
João VItor <jvitordiass@outlook.com>
2023-03-05 23:17:31 -0300

    refactor: remove header

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,7 +1,6 @@
 export { AppBar } from './AppBar'
 export { Category } from './Category'
 export { FormTextField } from './FormTextField'
-export { Header } from './Header'
 export { Loader } from './Loader'
 export { Layout } from  './Layout'
 export { Notification } from './Notification'
