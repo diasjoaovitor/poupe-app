8867199c5f5036246c8515f17e478fba1a991e4d
João Vitor <jvitordiass@outlook.com>
2023-03-10 17:58:50 -0300

    feat: add hook to save context

---
+++ src/shared/hooks/index.ts
@@ -0,0 +1,11 @@
+import { useAppContext } from '../contexts'
+
+export const useApp = () => {
+	const { appContext, saveContext } = useAppContext()
+
+  const handleNewTransaction = () => {
+    saveContext({ ...appContext, transaction: undefined })
+  }
+
+  return { handleNewTransaction }
+}
