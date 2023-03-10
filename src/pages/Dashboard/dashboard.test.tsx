60ea379ec907744142c0f8355d75400a99bcb2d5
João Vitor <jvitordiass@outlook.com>
2023-03-10 17:56:40 -0300

    test/refactor: Dashboard

--- src/pages/Dashboard/dashboard.test.tsx
+++ src/pages/Dashboard/dashboard.test.tsx
@@ -1,21 +1,24 @@
-import { fireEvent, render, screen  } from '@testing-library/react'
+import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { createMemoryRouter, RouterProvider } from 'react-router-dom'
 import { Dashboard } from '..'
-import { ThemeProvider } from '../../shared/contexts'
-import { useDashboard } from './useDashboard'
+import { AppBar } from '../../shared/components'
+import { AppProvider, ThemeProvider } from '../../shared/contexts'
 import { TTransaction } from '../../shared/types'
+import { getErrorMessage } from '../../shared/functions'
+import { useDashboardQuery } from './useDashboardQuery'
 
-jest.mock('./useDashboard')
+jest.mock('./useDashboardQuery')
 
-const mockedUseDashboard = useDashboard as jest.Mock<any> 
+const mockedUseDashboardQuery = useDashboardQuery as jest.Mock<any>
 
 const client = new QueryClient()
 
 const routeSetup = () => {
   const router = createMemoryRouter(
     [
-      { path: '/', element: <Dashboard /> }
+      { path: '/', element: <Dashboard /> },
+      { path: '/submit/:method', element: <AppBar md={true} page="AppBar" />}
     ],
     {
       initialEntries: ['/']
@@ -25,82 +28,22 @@ const routeSetup = () => {
   render(
     <ThemeProvider>
       <QueryClientProvider client={client}>
-        <RouterProvider router={router} />
+        <AppProvider>
+          <RouterProvider router={router} />
+        </AppProvider>
       </QueryClientProvider>
     </ThemeProvider>
   )
   
-  return { router }
+  return router
 }
 
-const mockSetup = (args: any) => {
-  mockedUseDashboard.mockImplementation(() => ({
+const mockSetup = () => {
+  mockedUseDashboardQuery.mockImplementation(() => ({
     isLoading: false,
-    period: {
-      month: 'Janeiro',
-      year: 2023
-    },
-    data: {
-      years: [2022, 2023],
-      transactions: [
-        {
-          category: 'Esporte',
-          date: '2023/01/06',
-          description: 'Uma transação',
-          period: 'Janeiro/2023',
-          ref: 'ref',
-          type: 'Despesa',
-          value: 50
-        },
-        {
-          category: 'Salário',
-          date: '2023/01/07',
-          description: 'Outra transação',
-          period: 'Janeiro/2023',
-          ref: 'ref',
-          type: 'Receita',
-          value: 100
-        }
-      ] as TTransaction[]
-    },
-    ...args
-  }))
-}
-
-describe('<Dashboard />', () => {
-  it('Exception message', () => {
-    const errorMessage = 'Algo deu Errado'
-    mockSetup({
-      message: errorMessage
-    })
-    routeSetup()
-    expect(screen.getByText(errorMessage)).toBeInTheDocument()
-  })
-
-  it('All Transactions', () => {
-    const handleTransactionClick = jest.fn()
-    mockSetup({
-      handleTransactionClick
-    })
-    routeSetup()
-
-    const transaction = screen.getByText('Uma transação')
-    const transaction2 = screen.getByText('Outra transação')
-
-    fireEvent.click(transaction)
-    expect(handleTransactionClick).toHaveBeenCalled()
-
-    fireEvent.click(transaction2)
-    expect(handleTransactionClick).toHaveBeenCalled()
-  })
-
-  it('One Transaction', () => {
-    const handleUpdate = jest.fn()
-    const handleDelete = jest.fn()
-    mockSetup(({
-      handleUpdate,
-      handleDelete,
-      transaction: {
+    error: null,
+    transactions: [
+      {
         category: 'Esporte',
         date: '2023/01/06',
         description: 'Uma transação',
@@ -108,17 +51,86 @@ describe('<Dashboard />', () => {
         ref: 'ref',
         type: 'Despesa',
         value: 50
+      },
+      {
+        category: 'Salário',
+        date: '2023/01/07',
+        description: 'Outra transação',
+        period: 'Janeiro/2023',
+        ref: 'ref',
+        type: 'Receita',
+        value: 100
       }
-    }))
-    routeSetup()
+    ] as TTransaction[],
+    years: [ 2022, 2023, 2023, 2021 ]
+  }))
+}
 
-    const update = screen.getByText('Editar')
-    const del = screen.getByText('Excluir')
-    
-    fireEvent.click(update)
-    expect(handleUpdate).toHaveBeenCalled()
+const mockErrorSetup = () => {
+  mockedUseDashboardQuery.mockImplementation(() => ({
+    isLoading: false,
+    error: true,
+    transactions: [],
+    years: [ 2023 ]
+  }))
+}
 
-    fireEvent.click(del)
-    expect(handleDelete).toHaveBeenCalled()
+const openModal = (description: string) => {
+  const transaction = screen.getByText(description)
+  fireEvent.click(transaction)
+}
+
+const closeModal = () => {
+  const modal = screen.getByTestId('transaction')
+  const close = modal.querySelector('button') as HTMLButtonElement
+  fireEvent.click(close)
+}
+
+const navigateToUpdate = () => {
+  const update = screen.getByText('Editar')
+  fireEvent.click(update)
+}
+
+const navigateToDashboard = () => {
+  const dashboard = screen.getByText('Dashboard')
+  fireEvent.click(dashboard)
+}
+
+describe('<Dashboard />', () => {
+  it('Open Transaction modal', async () => {
+    mockSetup()
+    await act(() => {
+      routeSetup()
+    })
+    const closedModal = screen.queryByTestId('transaction')
+    expect(closedModal).not.toBeInTheDocument()
+    openModal('Uma transação')
+    const openedModal = screen.queryByTestId('transaction')
+    expect(openedModal).toBeInTheDocument()
+  })
+
+  it('Navigate and save context and close modal', async () => {
+    mockSetup()
+    const router = await act(() => routeSetup())
+    openModal('Outra transação')
+    navigateToUpdate()
+    expect(router.state.location.pathname).toBe('/submit/update') 
+    navigateToDashboard()
+    expect(router.state.location.pathname).toBe('/') 
+    closeModal()
+    await waitFor(() => {
+      expect(screen.queryByTestId('transaction')).not.toBeInTheDocument()
+    })
+  })
+
+  it('Exception message', async () => {
+    mockErrorSetup()
+    await act(() => {
+      routeSetup()
+    })
+    const message = getErrorMessage('generic')
+    await waitFor(() => {
+      expect(screen.getByText(message)).toBeInTheDocument()
+    })
   })
 }) 
