e238d189932a793a31c3c60523900dc36c135ef2
João VItor <jvitordiass@outlook.com>
2023-03-22 22:22:59 -0300

    refactor: separate hook in new file

---
+++ src/shared/contexts/AppContext/index.tsx
@@ -0,0 +1,26 @@
+import { createContext, ReactNode, useContext } from 'react'
+import { TAppData } from '../../types'
+import { useAppContextHook } from './useAppContextHook'
+
+type TAppContext = {
+  appContext: TAppData
+  fetchEnabledContext: boolean
+  setFetchEnabledContext(enabled: boolean): void
+  saveContext(appContext: TAppData): void
+  clearContext(): void
+}
+
+const AppContext = createContext({} as TAppContext)
+
+export const useAppContext = () => useContext(AppContext)
+
+export const AppProvider: React.FC<{children: ReactNode}> = ({ children }) => {
+  const provider = useAppContextHook()
+  return (
+    <AppContext.Provider 
+      value={provider}
+    >
+     {children}
+    </AppContext.Provider>
+  )
+}
---
+++ src/shared/contexts/AppContext/useAppContextHook.ts
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { defaultAppState } from '../../states'
+import { TAppData } from '../../types'
+
+export const useAppContextHook = () => {
+  const [ appContext, setAppContext ] = useState(defaultAppState)
+  const [ fetchEnabledContext, setFetchEnabledContext ] = useState(true)
+
+  const saveContext = (data: TAppData) => {
+    setAppContext(data)
+    setFetchEnabledContext(false)
+  }
+
+  const clearContext = () => {
+    setAppContext(defaultAppState)
+    setFetchEnabledContext(true)
+  }
+
+  return {
+    appContext, fetchEnabledContext, 
+    setFetchEnabledContext, saveContext, clearContext
+  }
+}
