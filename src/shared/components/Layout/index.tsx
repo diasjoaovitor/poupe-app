0cfe13290451c5008214d78b8c72fc4d27d7fc55
João VItor <jvitordiass@outlook.com>
2023-03-22 22:16:34 -0300

    feat: add <ComeBack /> and <Snackbar />

--- src/shared/components/Layout/index.tsx
+++ src/shared/components/Layout/index.tsx
@@ -1,7 +1,8 @@
 import { ReactNode } from 'react'
-import { Box, Typography, useMediaQuery, useTheme } from '@mui/material'
-import { AppBar, Loader, Notification } from '..'
+import { Box, Stack, Typography, useMediaQuery, useTheme } from '@mui/material'
+import { AppBar, ComeBack, Loader, Notification, Snackbar } from '..'
 import { TMUIColor } from '../../types'
+import * as GS from '../../styles'
 import * as S from './style'
 
 type Props = {
@@ -9,13 +10,16 @@ type Props = {
   children: ReactNode
   isLoading: boolean
   notificationMessage: string
+  successMessage: string
   color: {
     title: string
     mui: TMUIColor
   }
 }
 
-export const Layout: React.FC<Props> = ({ page, children, isLoading, notificationMessage, color }) => {
+export const Layout: React.FC<Props> = ({ 
+  page, children, isLoading, notificationMessage, successMessage, color 
+}) => {
   const theme = useTheme()
   const md = useMediaQuery(theme.breakpoints.up('md'))
 
@@ -24,14 +28,18 @@ export const Layout: React.FC<Props> = ({ page, children, isLoading, notificatio
       <AppBar page={page} md={md} color={color.mui} />
       <Box component="main">
         {md && (
-          <Typography component="h1" variant="h5" sx={{ '&::after': { backgroundColor: color.title } }}>
-            {page}
-          </Typography>
+          <Stack sx={GS.FlexBetween}>
+            <Typography component="h1" variant="h5" sx={{ '&::after': { backgroundColor: color.title } }}>
+              {page}
+            </Typography>
+            <ComeBack color={color.mui} />
+          </Stack>
         )}
         {children}
       </Box>
       <Loader open={isLoading} color={color.mui} />
 			<Notification message={notificationMessage} />
+      <Snackbar message={successMessage} />
     </Box>
   ) 
 }
