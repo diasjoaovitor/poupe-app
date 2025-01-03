0a10bf8f984f4fa93e95a0a73b8141ee93b0f293
João Vitor <jvitordiass@outlook.com>
2025-01-03 16:50:08 -0300

    chore: update `react-app` to `vite` and all project dependencies
    - update .env.example and firebase config
    - update index.html
    - update tsconfig.json
    - add vite.config.ts

--- src/shared/environment/firebase.ts
+++ src/shared/environment/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY as string,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN as string,
-  databaseURL: process.env.REACT_APP_DATABASE_URL as string,
-  projectId: process.env.REACT_APP_PROJECT_ID as string,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID as string,
-  appId: process.env.REACT_APP_APP_ID as string
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID
 })
 
 export const authConfig = getAuth(app)
